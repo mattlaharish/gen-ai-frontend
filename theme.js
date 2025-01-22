dd2af65aba6436866fb0bd131a93302aefc92a54
mattlaharish <mattlaharish@technavio.com>
2025-01-23 02:01:41 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -9,7 +9,7 @@ const openNewChat = () => document.getElementById('new-chat-button').click();
 document.addEventListener('DOMContentLoaded', function () {
     // Add a delay before simulating the click
     setTimeout(function () {
-        let newChatButton = `<div class="MuiBox-root css-0"><button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk" tabindex="0" type="button" id="custom-new-upload-button"><a href="http://127.0.0.1:5000/"><svg xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 512 512" id="upload"><path fill="white" d="M398.1 233.2c0-1.2.2-2.4.2-3.6 0-65-51.8-117.6-115.7-117.6-46.1 0-85.7 27.4-104.3 67-8.1-4.1-17.2-6.5-26.8-6.5-29.5 0-54.1 21.9-58.8 50.5C57.3 235.2 32 269.1 32 309c0 50.2 40.1 91 89.5 91H224v-80h-48.2l80.2-83.7 80.2 83.6H288v80h110.3c45.2 0 81.7-37.5 81.7-83.4 0-45.9-36.7-83.2-81.9-83.3z"></path></svg> Upload</a></button></div>`;
+        let newChatButton = `<div class="MuiBox-root css-0"><button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk" tabindex="0" type="button" id="custom-new-upload-button"><a href="https://policyai.quantzig.com/puma-upload"><svg xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 512 512" id="upload"><path fill="white" d="M398.1 233.2c0-1.2.2-2.4.2-3.6 0-65-51.8-117.6-115.7-117.6-46.1 0-85.7 27.4-104.3 67-8.1-4.1-17.2-6.5-26.8-6.5-29.5 0-54.1 21.9-58.8 50.5C57.3 235.2 32 269.1 32 309c0 50.2 40.1 91 89.5 91H224v-80h-48.2l80.2-83.7 80.2 83.6H288v80h110.3c45.2 0 81.7-37.5 81.7-83.4 0-45.9-36.7-83.2-81.9-83.3z"></path></svg> Upload</a></button></div>`;
         insertAfter(document.querySelector('.css-14k6mw7 img'), newChatButton);
 
     }, 1000);
